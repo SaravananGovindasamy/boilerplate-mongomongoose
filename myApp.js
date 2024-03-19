672a3551f7c16c0ef041c6512ff41e4d5a1537f6
SaravananGovindasamy <161823765+SaravananGovindasamy@users.noreply.github.com>
2024-03-20 00:06:31 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -12,6 +12,16 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     console.error('Error connecting to MongoDB Atlas:', error);
   });
 
+  const personSchema = new mongoose.Schema({
+    name: { type: String, required: true }, // Required field of type String
+    age: Number, // Field of type Number
+    favoriteFoods: [String] // Field of type array of Strings
+  });
+  
+  // Create model from personSchema and assign it to the existing variable Person
+  Person = mongoose.model('Person', personSchema);
+
+
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
